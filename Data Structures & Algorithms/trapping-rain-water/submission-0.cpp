056efe800d1de3f56a2a14bb11e747f43dddb688
Priyanshu Mishra <themishra5555@gmail.com>
2026-07-22 17:56:55 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int leftmax=0,water=0,rightmax=0,max=height[0],index=0;
+        int n=height.size();
+        //finding max height
+        for(int i=0;i<n;i++){
+            if(height[i]>max){
+                max=height[i];
+                index=i;
+            }
+        }
+        //left part 
+        for(int i=0;i<index;i++){
+            if(leftmax>height[i]){
+                water+=leftmax-height[i];
+            }
+            else
+            leftmax=height[i];
+        }
+        //right part
+        for(int i=n-1;i>index;i--){
+            if(rightmax>height[i]){
+                water+=rightmax-height[i];
+            }
+            else
+            rightmax=height[i];
+        }
+         return water;
+    }
+};
