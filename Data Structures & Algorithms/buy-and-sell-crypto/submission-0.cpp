2854014e3e72e8d29a192f0d754b6823c7b06ee7
Priyanshu Mishra <themishra5555@gmail.com>
2026-07-23 21:25:36 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=prices[0];
+        int profit=0;
+
+        for(int i=0;i<prices.size();i++)
+        {
+            mini=min(mini,prices[i]);
+
+            if(prices[i]>mini) {
+            profit=max(profit,prices[i]-mini);
+            }
+        }
+
+        return profit;
+    }
+};
